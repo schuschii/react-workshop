6385ea08670d2d75a553a6aae9bbad11d21c37a3
z8uz81 <159907392+schuschii@users.noreply.github.com>
2024-08-12 13:39:37 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// use nextId() to generate a new unique ID when needed
+import nextId from "./generateId";
+
+function App() {
+  return (
+    <>
+      <p>Hello from the App!</p>
+    </>
+  );
+}
+
+export default App;
